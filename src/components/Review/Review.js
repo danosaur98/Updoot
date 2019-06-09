f5d3baaeca433becb0f1f28f14276faaf9388bc4
danosaur98 <daniel.zhou2012@gmail.com>
2019-06-09 18:18:00 -0400

    working on payments

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -7,105 +7,170 @@ import StarRatingComponent from 'react-star-rating-component'
 class Review extends Component {
 
     constructor(props) {
+        //TODO: finish verified code logic
         super(props)
         this.state = {
             value: '',
             code: '',
             like: '',
             dislike: '',
+            codeMessage: '',
+            likeMessage: '',
+            dislikeMessage: '',
+            ratingMessage: '',
             //   app: `${props.protocol}//${props.realm}`,
             //   rulerUsername: props.ruler,
             clickAdd: false,
             rating: 5,
+            verified: false,
         }
         this.userSession = new UserSession({ appConfig })
-        this.finishReview = this.finishReview.bind(this)
+        this.finishCode = this.finishCode.bind(this)
+        this.finishLike = this.finishLike.bind(this)
+        this.finishDislike = this.finishDislike.bind(this)
+
         this.handleChangeCode = this.handleChangeCode.bind(this);
         this.handleChangeLike = this.handleChangeLike.bind(this);
         this.handleChangeDislike = this.handleChangeDislike.bind(this);
     }
-    finishReview(e) {
+
+    finishCode(e) {
         e.preventDefault();
-        console.log("finished!");
+        this.setState({
+            verified: true,
+            codeMessage: 'Verified! Submit your review for La Mesa below.'
+        });
     }
+    finishLike(e) {
+        e.preventDefault();
+        if (this.state.verified) {
+            this.setState({
+                likeMessage: 'Thanks for sharing! You just earned 300 Satoshi!',
+            });
+        }
+        else {
+            this.setState({
+                likeMessage: 'Please enter a valid unique code above.'
+            });
+        }
+    }
+    finishDislike(e) {
+        e.preventDefault();
+        if (this.state.verified) {
+
+            this.setState({
+                dislikeMessage: 'Thanks for sharing! You just earned 300 Satoshi!',
+            });
+        }
+        else {
+            this.setState({
+                dislikeMessage: 'Please enter a valid unique code above.'
+            });
+        }
+    }
+
     onStarClick(nextValue, prevValue, name) {
-        this.setState({ rating: nextValue });
+        if (this.state.verified) {
+            this.setState({
+                rating: nextValue,
+                ratingMessage: 'Thanks for rating! You just earned 100 Satoshi!'
+            });
+        }
+        else {
+            this.setState({
+                ratingMessage: 'Please enter a valid unique code above.'
+            });
+        }
+
     }
     handleChangeCode(event) {
         console.log(event.target);
-        this.setState({ code: event.target.value });
+        this.setState({
+            code: event.target.value,
+        });
     }
     handleChangeLike(event) {
-        this.setState({ like: event.target.value });
+        this.setState({
+            like: event.target.value,
+        });
     }
     handleChangeDislike(event) {
-        this.setState({ dislike: event.target.value });
+        this.setState({
+            dislike: event.target.value,
+        });
     }
     render() {
         const { rating } = this.state;
 
         return (
             <div className="Review container">
-                <h2>
-                    Please enter your unique code.
-                </h2>
-                <form onSubmit={this.finishReview} className="input-group">
-                    <input
-                        className="form-control"
-                        onChange={this.handleChangeCode}
-                        value={this.state.code}
-                        required
-                        placeholder="123456789"
+                <div className="Review section">
+                    <h2>
+                        Please enter your unique code.
+                    </h2>
+                    <form onSubmit={this.finishCode} className="input-group">
+                        <input
+                            className="form-control"
+                            onChange={this.handleChangeCode}
+                            value={this.state.code}
+                            required
+                            placeholder="123456789"
+                        />
+                        <div className="input-group-append">
+                            <input type="submit" className="btn btn-primary" value="Submit" />
+                        </div>
+                    </form>
+                    {this.state.codeMessage}
+                    <br />
+                </div>
+                <div className="Review section">
+                    <h3>How was your experience?</h3>
+                    <StarRatingComponent
+                        name="rate1"
+                        starCount={5}
+                        value={rating}
+                        onStarClick={this.onStarClick.bind(this)}
                     />
-                    <div className="input-group-append">
-                        <input type="submit" className="btn btn-primary" value="Submit" />
-                    </div>
-                </form>
-                <br />
-
-                <h3>How was your experience?</h3>
-                <StarRatingComponent
-                    name="rate1"
-                    starCount={5}
-                    value={rating}
-                    onStarClick={this.onStarClick.bind(this)}
-                />
-
-                <br />
-
-                <h3>
-                    What did you like?
+                    <br />
+                    {this.state.ratingMessage}
+                </div>
+                <div className="Review section">
+                    <h3>
+                        What did you like?
+                     </h3>
+                    <form onSubmit={this.finishLike} className="input-group">
+                        <input
+                            className="form-control"
+                            onChange={this.handleChangeLike}
+                            value={this.state.like}
+                            required
+                            placeholder="Service was amazing!"
+                        />
+                        <div className="input-group-append">
+                            <input type="submit" className="btn btn-primary" value="Submit" />
+                        </div>
+                    </form>
+                    {this.state.likeMessage}
+                    <br />
+                </div>
+                <div className="Review section">
+                    <h3>
+                        What didn't you like?
                 </h3>
-                <form onSubmit={this.finishReview} className="input-group">
-                    <input
-                        className="form-control"
-                        onChange={this.handleChangeLike}
-                        value={this.state.like}
-                        required
-                        placeholder="Service was amazing!"
-                    />
-                    <div className="input-group-append">
-                        <input type="submit" className="btn btn-primary" value="Submit" />
-                    </div>
-                </form>
-
-                <br />
-
-                <h3>
-                    What didn't you like?
-                </h3>
-                <form onSubmit={this.finishReview} className="input-group">
-                    <input
-                        className="form-control"
-                        onChange={this.handleChangeDislike}
-                        value={this.state.dislike}
-                        required
-                        placeholder="Waiting time was a little long."
-                    />
-                    <div className="input-group-append">
-                        <input type="submit" className="btn btn-primary" value="Submit" />
-                    </div>
-                </form>
+                    <form onSubmit={this.finishDislike} className="input-group">
+                        <input
+                            className="form-control"
+                            onChange={this.handleChangeDislike}
+                            value={this.state.dislike}
+                            required
+                            placeholder="Waiting time was a little long."
+                        />
+                        <div className="input-group-append">
+                            <input type="submit" className="btn btn-primary" value="Submit" />
+                        </div>
+                    </form>
+                    {this.state.dislikeMessage}
+                </div>
             </div>
         );
     }
